ee6ba4402b6dbe98486ca9c791866842b0cddb49
zekth <vince.legoff@gmail.com>
2019-07-30 13:40:48 +0200

    add measures + refresh readme

---
+++ src/components/rulerMeasures.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <section class="section">
+      <div class="content">
+        <div class="columns">
+          <div class="column is-half-desktop">
+            <h3>Measures</h3>
+            <table class="table is-striped">
+              <thead>
+                <tr>
+                  <th>Units</th>
+                  <th>mm</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>18</td>
+                </tr>
+                <tr>
+                  <td>1.25</td>
+                  <td>22.5</td>
+                </tr>
+                <tr>
+                  <td>1.5</td>
+                  <td>27</td>
+                </tr>
+                <tr>
+                  <td>1.75</td>
+                  <td>31.5</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>36</td>
+                </tr>
+                <tr>
+                  <td>2.25</td>
+                  <td>40.5</td>
+                </tr>
+                <tr>
+                  <td>2.75</td>
+                  <td>49.5</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>54</td>
+                </tr>
+                <tr>
+                  <td>6</td>
+                  <td>114</td>
+                </tr>
+                <tr>
+                  <td>6.25</td>
+                  <td>112.5</td>
+                </tr>
+                <tr>
+                  <td>7</td>
+                  <td>132</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
